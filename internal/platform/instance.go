5df7c0347181b1884fd8d131071a8c6fb2b3f758
6hx0ube8j9 <84956360+6hx0ube8j9@users.noreply.github.com>
2026-09-18 15:01:37 +0800

    Create instance.go

---
+++ internal/platform/instance.go
@@ -0,0 +1,37 @@
+package platform
+
+import (
+	"syscall"
+	"unsafe"
+
+	"github.com/tailscale/win"
+)
+
+var procCreateMutex = syscall.NewLazyDLL("kernel32.dll").NewProc("CreateMutexW")
+
+type InstanceLock struct{ handle win.HANDLE }
+
+func AcquireSingleInstance(mutexName, windowTitle string) (*InstanceLock, bool) {
+	const ERROR_ALREADY_EXISTS = 183
+	namePtr, _ := syscall.UTF16PtrFromString(mutexName)
+	hMutex, _, _ := procCreateMutex.Call(0, 1, uintptr(unsafe.Pointer(namePtr)))
+	if syscall.GetLastError() == syscall.Errno(ERROR_ALREADY_EXISTS) {
+		if hMutex != 0 {
+			win.CloseHandle(win.HANDLE(hMutex))
+		}
+		titlePtr, _ := syscall.UTF16PtrFromString(windowTitle)
+		if hwnd := win.FindWindow(nil, titlePtr); hwnd != 0 {
+			win.ShowWindow(hwnd, win.SW_RESTORE)
+			win.SetForegroundWindow(hwnd)
+		}
+		return nil, false
+	}
+	return &InstanceLock{handle: win.HANDLE(hMutex)}, true
+}
+
+func (l *InstanceLock) Release() {
+	if l != nil && l.handle != 0 {
+		win.CloseHandle(l.handle)
+		l.handle = 0
+	}
+}
