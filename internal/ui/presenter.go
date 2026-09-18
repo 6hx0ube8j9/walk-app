25015015bc376af96c71acc15bc5712e68cb61cd
6hx0ube8j9 <84956360+6hx0ube8j9@users.noreply.github.com>
2026-09-18 14:58:05 +0800

    Create presenter.go

---
+++ internal/ui/presenter.go
@@ -0,0 +1,32 @@
+package ui
+
+import (
+	"myapp/internal/core"
+
+	"github.com/tailscale/walk"
+)
+
+type WalkPresenter struct {
+	mw *walk.MainWindow
+}
+
+func NewWalkPresenter() *WalkPresenter {
+	return &WalkPresenter{}
+}
+
+func (p *WalkPresenter) SetWindow(mw *walk.MainWindow) {
+	p.mw = mw
+}
+
+// ShowError 落地实现 core.DialogPresenter，保证在 Win32 UI 线程弹出
+func (p *WalkPresenter) ShowError(title, message string) {
+	if p.mw == nil {
+		ShowErrorDialog(nil, title, message)
+		return
+	}
+	p.mw.Synchronize(func() {
+		ShowErrorDialog(p.mw, title, message)
+	})
+}
+
+var _ core.DialogPresenter = (*WalkPresenter)(nil)
